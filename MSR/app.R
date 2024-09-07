2c09ed4ada1be73b10a26ed666a5e9d22bf8a04c
klabrx <klaus@brueckner.name>
2024-09-07 14:22:35 +0200

    Größe läuft durch bis zum Report

--- MSR/app.R
+++ MSR/app.R
@@ -1,11 +1,14 @@
 library(shiny)
 library(shinyjs)
 
+# Lade das vorbereitende Skript, das alle nötigen Daten und Funktionen bereitstellt
+source("./scripts/prepApp.r")
+
 # UI
 ui <- fluidPage(
   useShinyjs(),
   
-  # Einbindung der externen CSS-Datei
+  # Einbindung der externen CSS-Datei (Falls vorhanden)
   includeCSS("styles/styles.CSS"),
   
   # Fixierter Header
@@ -16,15 +19,15 @@ ui <- fluidPage(
       fluidRow(
         column(4, 
                h3("Eingabefelder"),
-               # Platzhalter für Eingabefelder
-               textInput("input1", "Eingabefeld 1", value = ""),
-               textInput("input2", "Eingabefeld 2", value = ""),
-               actionButton("submit", "Abschicken")
+               # Eingabefeld für Wohnungsgröße, begrenzt an die Referenztabelle
+               numericInput("wohn_groesse_input", "Wohnungsgröße in m²", 
+                            value = NA, min = min(reference_table$von), 
+                            max = max(reference_table$bis_unter), step = 0.1)
         ),
         column(8, 
                h3("Result-Report"),
-               # Platzhalter für Ergebnisbericht
-               verbatimTextOutput("report")
+               # Dynamischer Report, der mit renderUI generiert wird
+               uiOutput("report_ui")
         )
       )
   ),
@@ -36,10 +39,36 @@ ui <- fluidPage(
 # Server
 server <- function(input, output, session) {
   
-  # Dynamischer Ergebnisbericht
-  output$report <- renderText({
-    paste("Ergebnis 1:", input$input1, "\n",
-          "Ergebnis 2:", input$input2)
+  # Dynamischer Ergebnisbericht, der automatisch aktualisiert wird
+  output$report_ui <- renderUI({
+    
+    # Prüfen, ob eine Wohnungsgröße angegeben wurde
+    if (is.na(input$wohn_groesse_input)) {
+      return(HTML("<table class='table'>
+                  <tr><th>Merkmal</th><th>Angaben</th><th>Von</th><th class='highlight'>Ortsüblich</th><th>Bis</th></tr>
+                  <tr><td>Wohnungsgröße</td><td>Pflichtangabe fehlt</td><td></td><td class='highlight'></td><td></td></tr>
+                  </table>"))
+      
+    } else {
+      # Wohnungsgröße aus dem Input
+      wohn_groesse <- input$wohn_groesse_input
+      
+      # Finde den Bereich in der Referenztabelle
+      row <- reference_table[reference_table$von <= wohn_groesse & reference_table$bis_unter > wohn_groesse, ]
+      
+      if (nrow(row) == 1) {
+        return(HTML(paste0("<table class='table'>
+                           <tr><th>Merkmal</th><th>Angaben</th><th>Von</th><th class='highlight'>Ortsüblich</th><th>Bis</th></tr>
+                           <tr><td>Wohnungsgröße</td><td>von ", row$von, " bis unter ", row$bis_unter, " m²</td><td>", 
+                           row$low, "</td><td class='highlight'>", row$med, "</td><td>", row$hi, "</td></tr>
+                           </table>")))
+      } else {
+        return(HTML("<table class='table'>
+                    <tr><th>Merkmal</th><th>Angaben</th><th>Von</th><th class='highlight'>Ortsüblich</th><th>Bis</th></tr>
+                    <tr><td>Wohnungsgröße</td><td>Kein passender Bereich gefunden</td><td></td><td class='highlight'></td><td></td></tr>
+                    </table>"))
+      }
+    }
   })
 }
 
---
+++ MSR/scripts/prepApp.R
@@ -0,0 +1,6 @@
+# ./script/prepApp.R
+
+# Laden der Referenztabelle
+reference_table <- read.csv("./data/RefTab_Groesse.csv", sep = ";", stringsAsFactors = FALSE)
+
+# Eventuell weitere Vorbereitungen wie das Laden anderer Daten oder Einstellungen
